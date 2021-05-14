ab6155e09fde1da3cf5939d129b8f2fc02258037
DFranke <franke@edf-online.de>
2021-05-14 22:10:00 +0200

    SqueezeNumbers for Hexadecimals

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/view/LineLayout.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/view/LineLayout.java
@@ -305,18 +305,11 @@ abstract class LineLayout {
       GroupNumbers groupNumbers = fontInfo.getGroupNumbers();
       if (groupNumbers != GroupNumbers.NONE) {
         while (to > from) {
-          int count = 0;
-          int left = to - from;
-          boolean hex = left > 3 && chars[from] == '0' && (chars[from + 1] == 'x' || chars[from + 1] == 'X');
-          if (hex) {
-            count = countHexSequence(chars, from + 2, to);
-            if (count >= 5)
-            {
-              chunk.fragments.add(TextFragmentFactory.createTextFragment(chars, from, from + 2, isRtl, fontInfo));
-              from +=2;
-              chunk.fragments.add(new ComplexTextFragment(chars, from, from + count, isRtl, fontInfo, 4));
-              from += count;
-              count = 0;
+          int count = countDigitSequence(chars, from, to);
+          if (count >= 4) {
+            chunk.fragments.add(new ComplexTextFragment(chars, from, from + count, isRtl, fontInfo, 3));
+            from += count;
+            count = 0;
             }else{
               count = 2;
             }
